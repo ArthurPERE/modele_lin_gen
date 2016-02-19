b25fdb5536d3ba4ccdd1033317de219608acb6c8
ArthurPERE <arthur.pere@insa-lyon.fr>
2016-02-19 15:54:46 +0100

    continuation du TP

--- TP_GLM.R
+++ TP_GLM.R
@@ -21,3 +21,14 @@ for (i in 1:length(tab[,2])){
 }
 
 tab2 = as.data.frame(cbind(age, alcool, tabac, malade))
+
+# logit(P(M|alcool)) = b0 + b1*alcool
+glm1 = glm(malade~alcool, data=tab2, family='binomial')
+summary(glm1)
+# estimate alcool est >1 donc cette variable a un effet deletere 
+
+OR = exp(1.73)
+# b1 est distribuer normalement donc IC = b1 ± sqrt(var(b1)), sqrt(var(b1)) = std.error
+
+exp(1.73 + 1.96 * 0.1752)
+exp(1.73 - 1.96 * 0.1752)
