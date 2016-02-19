f427cb2d78a6f9aacd3650effec19847fdb2c272
ArthurPERE <arthur.pere@insa-lyon.fr>
2016-02-19 15:11:42 +0100

    fin ex3

--- TP1_ex3.R
+++ TP1_ex3.R
@@ -11,3 +11,19 @@ OR = 29*109/(70*19)
 # OR = 2.38 [1.24 4.66]
 
 # on a une augmentation des risques quand on prend la pillule contraceptive
+# ainsi que l'age
+
+# mais plus l'age augmente moins les femmes prennent la pillule
+
+tab = array(c(8,44,2,50,21,26,17,59), dim=c(2,2,2))
+
+mantelhaen.test(tab)
+
+###### Question 4
+
+# log odds = b0 + b1 co + b2 age
+# log P(IDM=1|CO, age) = b0 + b1 co + b2 age
+# log P(IDM=1|CO=0, age=0) = b0
+
+
+# le modele 2 inclu un autre terme qui est l'interaction
